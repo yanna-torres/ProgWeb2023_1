9b1b3532be817970a091582fc93dcc34998ec179
Yanna <torres.yanna03@gmail.com>
2023-10-06 14:03:59 -0300

    changes lab 05

---
+++ smd_ecommerce/src/java/config/Config.java
@@ -0,0 +1,9 @@
+
+package config;
+
+public class Config {
+    public static final String JDBC_DRIVER = "org.postgresql.Driver";
+    public static final String JDBC_URL = "jdbc:postgresql://localhost:5432/smdecommerce";
+    public static final String JDBC_USUARIO = "postgres";
+    public static final String JDBC_SENHA = "ufc123";
+}
---
+++ smd_ecommerce/src/java/usuario/modelo/Usuario.java
@@ -0,0 +1,75 @@
+package usuario.modelo;
+
+/**
+ *
+ * @author Leonardo Oliveira Moreira
+ * 
+ * Classe que representa a entidade usuário
+ */
+public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String endereco;
+    private String email;
+    private String login;
+    private String senha;
+    private boolean administrador;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+    
+}
